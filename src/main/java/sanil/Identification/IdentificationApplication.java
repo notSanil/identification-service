676ce848994342987a92a3f686f898ee5febc07c
notSanil <44319106+notSanil@users.noreply.github.com>
2024-05-24 11:52:18 +0530

    Initial commit

---
+++ src/main/java/sanil/Identification/IdentificationApplication.java
@@ -0,0 +1,13 @@
+package sanil.Identification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sanil/Identification/IdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package sanil.Identification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
