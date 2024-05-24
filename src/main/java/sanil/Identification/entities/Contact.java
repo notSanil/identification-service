ac1cc6ec84ce8e6d27a7354941aba27d2224e3e0
notSanil <44319106+notSanil@users.noreply.github.com>
2024-05-24 16:59:29 +0530

    Add additional validation to ddl

--- src/main/java/sanil/Identification/entities/Contact.java
+++ src/main/java/sanil/Identification/entities/Contact.java
@@ -1,11 +1,15 @@
 package sanil.Identification.entities;
 import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.*;
 import sanil.Identification.enums.LinkPrecedence;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name="contact")
+@SQLDelete(sql="UPDATE contact SET deleted_at=current_timestamp where id=?")
+@SQLRestriction("deleted_at is null")
 public class Contact {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -21,10 +25,12 @@ public class Contact {
     @Enumerated(EnumType.STRING)
     private LinkPrecedence linkPrecedence;
     @Column(name="created_at", nullable = false)
+    @CreationTimestamp(source = SourceType.DB)
     private LocalDateTime createdAt;
     @Column(name="updated_at", nullable = false)
+    @UpdateTimestamp(source = SourceType.DB)
     private LocalDateTime updatedAt;
     @Column(name="deleted_at")
-    private LocalDateTime deletedAt;
+    private LocalDateTime deletedAt = null;
 
 }
